175081bd03492dcd8b5e22bcf938a57cb8df927e
Burence1 <burensomondi@gmail.com>
2021-03-26 16:18:29 +0300

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { QuoteComponent } from './quote/quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details/quote-details.component';
 import { FormComponent } from './form/form/form.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    FormComponent
+    FormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
